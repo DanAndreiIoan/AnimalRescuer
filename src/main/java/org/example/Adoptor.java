5c765a7e4bf35f7e4ce61b96021769d120aafb1a
DanAndreiIoan <dan_andrei_ioan@yahoo.com>
2020-09-06 14:59:18 +0300

    set-up basic project structure

--- src/main/java/org/example/Adoptor.java
+++ src/main/java/org/example/Adoptor.java
@@ -19,20 +19,33 @@ public class Adoptor {
     public void setMoney(double money) {
         this.money = money;
     }
-public void feed(Animal animal, Food food){
-        System.out.println(this.name +" just gave some"+ food.getName() + "food to"+ animal.getName());
+    public void feed(Animal animal, Food food) {
+        System.out.println(this.name + " just gave some " + food.getName() + " food to " + animal.getName());
 
-    double hungerLevel = animal.getHungerLevel();
-    hungerLevel--;
-    animal.setHungerLevel(hungerLevel);
+        double hungerLevel = animal.getHungerLevel();
+        hungerLevel--;
+        animal.setHungerLevel(hungerLevel);
 
-    System.out.println(animal.getName() + "'s hunger level is now: " + animal.getHungerLevel());
-}
+        System.out.println(animal.getName() + "'s hunger level is now: " + animal.getHungerLevel());
+
+        if (food.getName().equals(animal.getFavoriteFoodName())) {
+            animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+            System.out.println(animal.getName() + "'s happiness level is now: " + animal.getHappinessLevel());
+        }
+    }
 
     public void entertain(Animal animal, EntertainmentActivity activity) {
         System.out.println(this.name + " just did/played some " + activity.getName() + " with " + animal.getName());
 
-        animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+        int increment;
+
+        if (activity.getName().equals(animal.getFavoriteActivityName())) {
+            increment = 2;
+        } else {
+            increment = 1;
+        }
+
+        animal.setHappinessLevel(animal.getHappinessLevel() + increment);
 
         System.out.println(animal.getName() + "'s happiness level is now: " + animal.getHappinessLevel());
     }
